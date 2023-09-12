f1591a1e360d684c4c4fb586037cb969b676326e
soufianbouktaib1@gmail.com <109437519+sofexbk@users.noreply.github.com>
2023-09-12 11:45:02 +0100

    table added

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,7 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit,ViewChild,AfterViewInit } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { EmpAddEditComponent } from './emp-add-edit/emp-add-edit.component';
 import { EmployeeService } from './services/employee.service';
+import {MatPaginator, MatPaginatorModule} from '@angular/material/paginator';
+import {MatSort, MatSortModule} from '@angular/material/sort';
+import {MatTableDataSource, MatTableModule} from '@angular/material/table';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
 
 @Component({
   selector: 'app-root',
@@ -9,7 +14,23 @@ import { EmployeeService } from './services/employee.service';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit {
-  title = 'crud-app';
+  //title = 'crud-app';
+  displayedColumns: string[] = [
+    'id', 
+    'firstName',
+   'lastName',
+    'email',
+    'dob',
+    'gender',
+    'education',
+    'company',
+    'experience',
+    'package'
+  ];
+  dataSource!: MatTableDataSource<any>;
+
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+  @ViewChild(MatSort) sort!: MatSort;
 
   constructor(private _dialog:MatDialog,private _empService:EmployeeService)
   {}
@@ -26,10 +47,22 @@ export class AppComponent implements OnInit {
   getEmployeeList(){
     this._empService.getEmployeeList().subscribe({
       next:(res)=>{
-       console.log(res)
+       this.dataSource=new MatTableDataSource(res);
+       this.dataSource.sort=this.sort;
+       this.dataSource.paginator=this.paginator;
       },
       error:console.log,//shorthand
     })
+  }
+
+
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+
+    if (this.dataSource.paginator) {
+      this.dataSource.paginator.firstPage();
+    }
   }
 
 
--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -17,6 +17,9 @@ import {MatRadioModule} from '@angular/material/radio';
 import {MatSelectModule} from '@angular/material/select';
 import { ReactiveFormsModule } from '@angular/forms';
 import {  HttpClientModule } from '@angular/common/http';
+import {MatTableModule} from '@angular/material/table';
+import {  MatPaginatorModule } from '@angular/material/paginator';
+import { MatSortModule } from '@angular/material/sort';
 
 @NgModule({
   declarations: [
@@ -38,7 +41,10 @@ import {  HttpClientModule } from '@angular/common/http';
     MatRadioModule,
     MatSelectModule,
     ReactiveFormsModule,
-    HttpClientModule
+    HttpClientModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatSortModule
   ],
   providers: [],
   bootstrap: [AppComponent]
